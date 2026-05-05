770655ad4647d1d730c5c4de566e72cbf0531e81
Luiz Felipe de Siqueira Goncalves <lfsg@cesar.school>
2026-05-05 11:35:17 -0300

    Comeco do arquivo inicial do jogo

---
+++ main.c
@@ -0,0 +1,92 @@
+#include "raylib.h"
+#include "musica_praieira.h"
+#include <math.h>
+#include <stdlib.h>
+
+#define LARGURA_TELA 800
+#define ALTURA_TELA 600
+#define Y_ALVO 500       
+#define RAIO_NOTA 30     
+#define VELOCIDADE 0.5f  
+
+int pontuacao = 0;
+char mensagem_feedback[20] = "";
+
+int verificar_pontuacao(int y_bola) {
+    int distancia = abs(y_bola - Y_ALVO);
+    float sobreposicao = 1.0f - ((float)distancia / (RAIO_NOTA * 2));
+    
+    if (sobreposicao >= 0.85f) { TextCopy(mensagem_feedback, "PERFEITO!"); return 300; }
+    if (sobreposicao >= 0.50f) { TextCopy(mensagem_feedback, "MUITO BOM!"); return 100; }
+    if (sobreposicao >= 0.25f) { TextCopy(mensagem_feedback, "OK!"); return 50; }
+    return 0;
+}
+
+int main() {
+    InitWindow(LARGURA_TELA, ALTURA_TELA, "Recife Beat - A Praieira");
+    InitAudioDevice();
+
+    GerarFaseCompleta();
+
+    Music musica = LoadMusicStream("a_praieira.ogg"); 
+    PlayMusicStream(musica);
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        UpdateMusicStream(musica);
+        float tempo_atual_ms = GetMusicTimePlayed(musica) * 1000.0f;
+
+        if (tempo_atual_ms >= 216000) break;
+
+        int teclas[] = { KEY_D, KEY_F, KEY_J, KEY_K };
+        for (int i = 0; i < 4; i++) {
+            if (IsKeyPressed(teclas[i])) {
+                for (int j = 0; j < total_notas; j++) {
+                    if (beatmap_a_praieira[j].ativa && beatmap_a_praieira[j].botao == i) {
+                        int y_atual = Y_ALVO - (beatmap_a_praieira[j].tempo_ms - tempo_atual_ms) * VELOCIDADE;
+                        int pontos = verificar_pontuacao(y_atual);
+                        if (pontos > 0) {
+                            pontuacao += pontos;
+                            beatmap_a_praieira[j].ativa = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            for (int i = 0; i < 4; i++) {
+                DrawRectangle(150 + i * 150, 0, 100, ALTURA_TELA, LIGHTGRAY);
+                DrawCircleLines(200 + i * 150, Y_ALVO, RAIO_NOTA + 2, BLACK);
+            }
+
+            for (int i = 0; i < total_notas; i++) {
+                if (beatmap_a_praieira[i].ativa) {
+                    float y_pos = Y_ALVO - (beatmap_a_praieira[i].tempo_ms - tempo_atual_ms) * VELOCIDADE;
+                    
+                    if (y_pos > ALTURA_TELA + 50) beatmap_a_praieira[i].ativa = false;
+                    
+                    if (y_pos > -50 && y_pos < ALTURA_TELA) {
+                        Color corNota = (tempo_atual_ms > 180000) ? ORANGE : MAROON;
+                        DrawCircle(200 + beatmap_a_praieira[i].botao * 150, (int)y_pos, RAIO_NOTA, corNota);
+                    }
+                }
+            }
+
+            DrawText(TextFormat("PONTOS: %06d", pontuacao), 20, 20, 25, DARKGRAY);
+            DrawText(mensagem_feedback, 350, 50, 30, GOLD);
+            DrawText("A PRAIEIRA - CHICO SCIENCE", 250, 15, 20, MAROON);
+
+        EndDrawing();
+    }
+
+    UnloadMusicStream(musica);
+    CloseAudioDevice();
+    CloseWindow();
+
+    return 0;
+}
