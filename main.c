790c21136903fc715728cd2ed71ff23b24013f3e
Eros Amancio Nascimento <ean@cesar.school>
2026-05-08 17:42:22 -0300

    Adição da lista encadeada para a musica.

--- main.c
+++ main.c
@@ -5,87 +5,171 @@
 
 #define LARGURA_TELA 800
 #define ALTURA_TELA 600
-#define Y_ALVO 500       
-#define RAIO_NOTA 30     
-#define VELOCIDADE 0.5f  
+#define Y_ALVO 500
+#define RAIO_NOTA 30
+#define VELOCIDADE 0.5f
 
 int pontuacao = 0;
 char mensagem_feedback[20] = "";
 
 int verificar_pontuacao(int y_bola) {
+
     int distancia = abs(y_bola - Y_ALVO);
+
     float sobreposicao = 1.0f - ((float)distancia / (RAIO_NOTA * 2));
-    
-    if (sobreposicao >= 0.85f) { TextCopy(mensagem_feedback, "PERFEITO!"); return 300; }
-    if (sobreposicao >= 0.50f) { TextCopy(mensagem_feedback, "MUITO BOM!"); return 100; }
-    if (sobreposicao >= 0.25f) { TextCopy(mensagem_feedback, "OK!"); return 50; }
+
+    if (sobreposicao >= 0.85f) {
+        TextCopy(mensagem_feedback, "PERFEITO!");
+        return 300;
+    }
+
+    if (sobreposicao >= 0.50f) {
+        TextCopy(mensagem_feedback, "MUITO BOM!");
+        return 100;
+    }
+
+    if (sobreposicao >= 0.25f) {
+        TextCopy(mensagem_feedback, "OK!");
+        return 50;
+    }
+
     return 0;
 }
 
 int main() {
+
     InitWindow(LARGURA_TELA, ALTURA_TELA, "Recife Beat - A Praieira");
+
     InitAudioDevice();
 
     GerarFaseCompleta();
 
-    Music musica = LoadMusicStream("a_praieira.ogg"); 
+    Music musica = LoadMusicStream("a_praieira.ogg");
+
     PlayMusicStream(musica);
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+
         UpdateMusicStream(musica);
+
         float tempo_atual_ms = GetMusicTimePlayed(musica) * 1000.0f;
 
-        if (tempo_atual_ms >= 216000) break;
+        if (tempo_atual_ms >= 216000)
+            break;
 
         int teclas[] = { KEY_LEFT, KEY_UP, KEY_DOWN, KEY_RIGHT };
+
         for (int i = 0; i < 4; i++) {
+
             if (IsKeyPressed(teclas[i])) {
-                for (int j = 0; j < total_notas; j++) {
-                    if (beatmap_a_praieira[j].ativa && beatmap_a_praieira[j].botao == i) {
-                        int y_atual = Y_ALVO - (beatmap_a_praieira[j].tempo_ms - tempo_atual_ms) * VELOCIDADE;
+
+                Nota *atual = inicio;
+
+                while (atual != NULL) {
+
+                    if (atual->ativa && atual->botao == i) {
+
+                        int y_atual = Y_ALVO - (atual->tempo_ms - tempo_atual_ms) * VELOCIDADE;
+
                         int pontos = verificar_pontuacao(y_atual);
+
                         if (pontos > 0) {
+
                             pontuacao += pontos;
-                            beatmap_a_praieira[j].ativa = false;
+                            atual->ativa = false;
                             break;
                         }
                     }
+
+                    atual = atual->prox;
                 }
             }
         }
 
         BeginDrawing();
+
             ClearBackground(RAYWHITE);
 
             for (int i = 0; i < 4; i++) {
+
                 DrawRectangle(150 + i * 150, 0, 100, ALTURA_TELA, LIGHTGRAY);
-                DrawCircleLines(200 + i * 150, Y_ALVO, RAIO_NOTA + 2, BLACK);
+
+                DrawCircleLines(
+                    200 + i * 150,
+                    Y_ALVO,
+                    RAIO_NOTA + 2,
+                    BLACK
+                );
             }
 
-            for (int i = 0; i < total_notas; i++) {
-                if (beatmap_a_praieira[i].ativa) {
-                    float y_pos = Y_ALVO - (beatmap_a_praieira[i].tempo_ms - tempo_atual_ms) * VELOCIDADE;
-                    
-                    if (y_pos > ALTURA_TELA + 50) beatmap_a_praieira[i].ativa = false;
-                    
+            Nota *atual = inicio;
+
+            while (atual != NULL) {
+
+                if (atual->ativa) {
+
+                    float y_pos = Y_ALVO - (atual->tempo_ms - tempo_atual_ms) * VELOCIDADE;
+
+                    if (y_pos > ALTURA_TELA + 50) {
+                        atual->ativa = false;
+                    }
+
                     if (y_pos > -50 && y_pos < ALTURA_TELA) {
-                        Color corNota = (tempo_atual_ms > 180000) ? ORANGE : MAROON;
-                        DrawCircle(200 + beatmap_a_praieira[i].botao * 150, (int)y_pos, RAIO_NOTA, corNota);
+
+                        Color corNota;
+
+                        if (tempo_atual_ms > 180000)
+                            corNota = ORANGE;
+                        else
+                            corNota = MAROON;
+
+                        DrawCircle(
+                            200 + atual->botao * 150,
+                            (int)y_pos,
+                            RAIO_NOTA,
+                            corNota
+                        );
                     }
                 }
+
+                atual = atual->prox;
             }
 
-            DrawText(TextFormat("PONTOS: %06d", pontuacao), 20, 20, 25, DARKGRAY);
-            DrawText(mensagem_feedback, 350, 50, 30, GOLD);
-            DrawText("A PRAIEIRA - CHICO SCIENCE", 250, 15, 20, MAROON);
+            DrawText(
+                TextFormat("PONTOS: %06d", pontuacao),
+                20,
+                20,
+                25,
+                DARKGRAY
+            );
+
+            DrawText(
+                mensagem_feedback,
+                350,
+                50,
+                30,
+                GOLD
+            );
+
+            DrawText(
+                "A PRAIEIRA - CHICO SCIENCE",
+                250,
+                15,
+                20,
+                MAROON
+            );
 
         EndDrawing();
     }
 
+    liberar_notas();
+
     UnloadMusicStream(musica);
+
     CloseAudioDevice();
+
     CloseWindow();
 
     return 0;
--- musica_praieira.h
+++ musica_praieira.h
@@ -2,57 +2,96 @@
 #define MUSICA_PRAIEIRA_H
 
 #include <raylib.h>
+#include <stdlib.h>
 
-typedef struct {
-    int tempo_ms;  
-    int botao;     
-    bool ativa;    
+typedef struct nota {
+    int tempo_ms;
+    int botao;
+    bool ativa;
+
+    struct nota *prox;
 } Nota;
 
-#define MAX_NOTAS 3000 
-static Nota beatmap_a_praieira[MAX_NOTAS];
-static int total_notas = 0;
+static Nota *inicio = NULL;
+
+void inserir_nota(int tempo, int botao) {
+    Nota *nova = malloc(sizeof(Nota));
+
+    nova->tempo_ms = tempo;
+    nova->botao = botao;
+    nova->ativa = true;
+    nova->prox = NULL;
+
+    if (inicio == NULL) {
+        inicio = nova;
+        return;
+    }
+
+    Nota *atual = inicio;
+
+    while (atual->prox != NULL) {
+        atual = atual->prox;
+    }
+
+    atual->prox = nova;
+}
+
+void liberar_notas() {
+    Nota *atual = inicio;
+
+    while (atual != NULL) {
+        Nota *temp = atual;
+        atual = atual->prox;
+        free(temp);
+    }
+
+    inicio = NULL;
+}
 
 void GerarFaseCompleta() {
     int tempo_base = 0;
-    total_notas = 0;
 
     while (tempo_base < 216000) {
-        
-       
+
         if (tempo_base < 20000) {
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 0,    1, true};
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 3000, 2, true};
-        } 
-        
-        else if (tempo_base < 180000) {
-           
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 0,    1, true};
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 576,  2, true};
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 1728, 2, true};
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 2448, 1, true};
-            
-            if (tempo_base % 12000 < 6000) {
-                beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 3600, 3, true}; 
-                beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 4896, 0, true};
-            } else {
-                beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 4320, 0, true};
-                beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 4896, 0, true};
-                beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 4896, 1, true};
-            }
-        }
-        
-        else {
-            
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 0,   1, true};
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 200, 2, true};
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 400, 1, true};
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 600, 2, true};
-            beatmap_a_praieira[total_notas++] = (Nota){tempo_base + 800, 3, true}; 
+
+            inserir_nota(tempo_base + 0, 1);
+            inserir_nota(tempo_base + 3000, 2);
+
         }
 
-        tempo_base += 6336; 
-        if (total_notas > MAX_NOTAS - 20) break; 
+        else if (tempo_base < 180000) {
+
+            inserir_nota(tempo_base + 0, 1);
+            inserir_nota(tempo_base + 576, 2);
+            inserir_nota(tempo_base + 1728, 2);
+            inserir_nota(tempo_base + 2448, 1);
+
+            if (tempo_base % 12000 < 6000) {
+
+                inserir_nota(tempo_base + 3600, 3);
+                inserir_nota(tempo_base + 4896, 0);
+
+            } else {
+
+                inserir_nota(tempo_base + 4320, 0);
+                inserir_nota(tempo_base + 4896, 0);
+                inserir_nota(tempo_base + 4896, 1);
+
+            }
+        }
+
+        else {
+
+            inserir_nota(tempo_base + 0, 1);
+            inserir_nota(tempo_base + 200, 2);
+            inserir_nota(tempo_base + 400, 1);
+            inserir_nota(tempo_base + 600, 2);
+            inserir_nota(tempo_base + 800, 3);
+
+        }
+
+        tempo_base += 6336;
     }
 }
 
